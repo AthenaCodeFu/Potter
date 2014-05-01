514720727dd01d61b5a9eb5448a3313ebaec380c
amacleay <amacleay@athenahealth.com>
2014-05-01 12:56:28 -0400

    Too much shit

--- Potter.pm
+++ Potter.pm
@@ -1,26 +1,55 @@
 package Potter;
 
+use Athena::Lib;
+
+use List::AllUtils qw(
+	any
+	sum
+);
+
+my $discount_map = {
+	1 => 0.00,
+	2 => 0.05,
+	3 => 0.10,
+	4 => 0.20,
+	5 => 0.25,
+};
 
 sub price {
 	my (@basket) = @_;
 
 	my $price = scalar(@basket) * 8;
 
-	my @unique_elements = keys {
-		map { $_ => 1 } @basket
-	};
+	my @sets_of_unique_books;
 
-	my $discount = {
-		1 => 0.00,
-		2 => 0.05,
-		3 => 0.10,
-		4 => 0.20,
-		5 => 0.25,
-	}->{scalar @unique_elements};
+	# book type : first index
+	# book itself: second index
+	my @grouped_books;
+	foreach my $book (@basket) {
+		push @{ $grouped_books[$book] }, $book;
+	}
 
-	$price *= (1.0 - $discount);
+	# go through lists of books
+	# and pull off each index
+	my @all_discount_groups;
+	while (any { @$_ } @grouped_books) {
+		my @group_of_unique_books;
+		foreach my $book_type (keys $discount_map) {
+			my $next_book = shift @{ $grouped_books[$book_type] };
+			push @group_of_unique_books, $next_book if $next_book;
+		}
+		push @all_discount_groups, \@group_of_unique_books
+			if @group_of_unique_books;
+	}
 
-	return $price;
+	my @group_costs;
+	foreach my $group (@all_discount_groups) {
+		my $discount = $discount_map->{scalar @$group};
+		my $group_cost = 8 * scalar @$group * (1 - $discount);
+		push @group_costs, $group_cost;
+	}
+
+	return sum @group_costs;
 }
 
 
