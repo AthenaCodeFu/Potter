c5e38d74d51c1ec4f92da131da0c8dd108213f2a
A.MacLeay <amacleay@athenahealth.com>
2014-05-01 12:33:45 -0400

    more

--- Potter.pm
+++ Potter.pm
@@ -4,7 +4,17 @@ package Potter;
 sub price {
 	my (@basket) = @_;
 
-	return scalar(@basket) * 8;
+	my $base_price = scalar(@basket) * 8;
+	my $real_price = $base_price;
+
+	my @unique_elements = keys {
+		map { $_ => 1 } @basket
+	};
+	if (@unique_elements == 2) {
+		$real_price = $real_price * 0.95;
+	}
+
+	return $real_price;
 }
 
 
--- t/Potter.t
+++ t/Potter.t
@@ -12,5 +12,8 @@ is Potter::price(1), 8;
 is Potter::price(1, 1), 16;
 is Potter::price(5), 8;
 is Potter::price(1, 2), 16 * .95;
+is Potter::price(1, 3), 16 * .95;
 
+is Potter::price(1,2,3), 24 * .9;
+is Potter::price(1,2,3,4,5), 40 * .75;
 done_testing();
